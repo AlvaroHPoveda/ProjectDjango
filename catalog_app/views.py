ca75462a9471fd086be1755ea972fb48249d9712
Alvaro Poveda <alvaropoveda@ingenieros.com>
2022-05-22 12:10:06 -0500

    filter

--- catalog_app/views.py
+++ catalog_app/views.py
@@ -4,11 +4,16 @@ from .models import Category
 from .serializers import BookSerializer
 from .permissions import IsOwnerOrReadOnly
 from rest_framework.permissions import IsAdminUser
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
 # Create your views here.
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend,filters.SearchFilter]
+    filterset_fields = ['color', 'cover']
+    search_fields = ['color']
 
     def get_permissions(self):
         if self.action == "create":
--- library_project/settings.py
+++ library_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'catalog_app',
     'rest_framework',
     'rest_framework_simplejwt',
+    'django_filters',
 ]
 
 
@@ -143,8 +144,10 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
-
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 3,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
 
 from datetime import timedelta
